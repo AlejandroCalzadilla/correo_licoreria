2d8627b7505e238e1f398b525e96acc24f7a4c68
andres <luis17suarez17@gmail.com>
2026-08-08 21:38:59 -0400

    credenciales_pago_facil

--- src/main/java/org/bebidas/infraestructure/servicioemail/PagoFacilGateway.java
+++ src/main/java/org/bebidas/infraestructure/servicioemail/PagoFacilGateway.java
@@ -67,9 +67,9 @@ public class PagoFacilGateway {
         }
 
         this.baseUrl = props.getProperty("PAGOFACIL_BASE_URL", "https://masterqr.pagofacil.com.bo/api/services/v2").trim();
-        this.tokenService = props.getProperty("PAGOFACIL_TOKEN_SERVICE", "").trim();
-        this.tokenSecret = props.getProperty("PAGOFACIL_TOKEN_SECRET", "").trim();
-        this.callbackUrl = props.getProperty("PAGOFACIL_CALLBACK_URL", "").trim();
+        this.tokenService = props.getProperty("PAGOFACIL_TOKEN_SERVICE", "a52063a1be51238364d1e55c3132f7c3b0625cfab2f37ac522a85cabaebdeb993bc8f601284fc35eee7f983e663d375c58f835b94a2aa558e8e2f9693885a2156598e1da9a7bf8caba2f3d6459f167fed3a50a375e0d80d517b560f04f017db078c3ee80e37f608537e752c82629095fbdcc6a4edcf4f22269c2f98f5d431c72").trim();
+        this.tokenSecret = props.getProperty("PAGOFACIL_TOKEN_SECRET", "eed967baf4b64e8980051c1b").trim();
+        this.callbackUrl = props.getProperty("PAGOFACIL_CALLBACK_URL", "https://9431-2800-cd0-42a1-6200-a56c-11f4-f4d6-c30e.ngrok-free.app/api/pagofacil/callback").trim();
         this.timeout = Integer.parseInt(props.getProperty("PAGOFACIL_TIMEOUT", "30").trim());
         this.enableLogs = Boolean.parseBoolean(props.getProperty("PAGOFACIL_ENABLE_LOGS", "true").trim());
 
